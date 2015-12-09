c1d96c1a5f032f18cf42bc91db6b9599d4c7dd4f
Graham Hough <graham.hough117@gmail.com>
2015-12-09 00:04:54 -0500

    updated menu more

--- main.cpp
+++ main.cpp
@@ -19,22 +19,34 @@ int main()
 		cout << "***********************************************************\nSELECTIONS:\n1. Read an expression\n2. Display the prefix expression\n3. Display the infix expression\n4. Display the postfix expression\n5. Evaluate the expression using postfix notation.\n6. Exit\n***********************************************************" << endl;
 		getline(cin,sel);
 
-		if(sel == "Q" || sel == "q")
+		if(sel == "6")
 		{
 			more = false;
 		}
+
 		if(sel == "1") {
 			cout << "Please type an infix expression: ";
 			getline(cin, infix);
                         exprTree.buildTree(infix);
 			exprTree.printTree();
 		}
+		if(sel == "3" && infix.size() != 0){
+			cout << "Expression from user: " << infix << endl;
+			cout << "INFIX Expression: " << infix << endl;
+		}
+
+		else{
+			cout << "Expression from user input is empty!" << endl;	
+		}
+		
 		if(sel == "4" && infix.size() != 0){
+			cout << "Expression from user input: " << infix << endl;
 			cout << "Outputting postifx:" << endl;
 			exprTree.postfix(exprTree.root);
 		}
+
 		else{
-			cout << "Please input an expression." << endl;	
+			cout << "Expression from user input is empty!" << endl;	
 		}
 			
 	}while (more);
